bfa5f92d23638c2d937a38822a9ab73f0e1fc21f
saipavant1 <saipavant@gmail.com>
2022-02-06 02:08:35 +0530

    Update hello-world.py

--- hello-world.py
+++ hello-world.py
@@ -3,6 +3,6 @@ import requests
 print("Om Sairam")
 print("Hello World")
 
-response = requests.post("https://httpbin.org/post")
+response = requests.post("https://3.209.99.235/post")
 print(response.status_code)
 print(response.content)
